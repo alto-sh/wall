1753b531c36a0585efec55461cc4936126c902c5
Solomon Jackson <sugarfi@sugarfi.dev>
2021-04-04 17:05:38 -0400

    heroku

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const SERVER = process.env['URL'] || 'http://localhost:8080';
+export const SERVER = 'https://whispering-fortress-29268.herokuapp.com';
 export const URL = /^https?:\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z0-9-](\/?(\??[a-zA-Z0-9-%]+)?)+$/;
 
