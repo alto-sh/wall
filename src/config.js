a723f11cb007351098ea0c9dcea91a177704b726
Solomon Jackson <sugarfi@sugarfi.dev>
2021-04-04 17:03:51 -0400

    heroku

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const SERVER = 'http://localhost:8080';
+export const SERVER = process.env['URL'] || 'http://localhost:8080';
 export const URL = /^https?:\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z0-9-](\/?(\??[a-zA-Z0-9-%]+)?)+$/;
 
