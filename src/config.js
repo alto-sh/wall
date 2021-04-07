35a4f8e8c009f85399fe582674abdd50b56331cc
Solomon Jackson <sugarfi@sugarfi.dev>
2021-04-07 08:48:59 -0400

    config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SERVER = 'https://wall-server.novata.ninja';
+export const SERVER = 'https://whispering-fortress-29268.herokuapp.com';
 export const URL = /^https?:\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z0-9-](\/?(\??[a-zA-Z0-9-%]+)?)+$/;
